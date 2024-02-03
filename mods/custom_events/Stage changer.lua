737bd1bb42ca9b7964cb2572fcc7865241ed1861
mcgagabe19 <egzozu.be.bas@gmail.com>
2024-02-03 17:35:25 +0300

    this also useless

--- mods/custom_events/Stage changer.lua
+++
@@ -1,8 +0,0 @@
-function onEvent(name, value1, value2)
-	if name == 'Stage changer' then
-addLuaScript('stages/'..value1)
-loadScript(value1, true)
-removeLuaScript('stages/'..value2)
-removeLuaSprite('stages/'..value2);
-	end
-	end
