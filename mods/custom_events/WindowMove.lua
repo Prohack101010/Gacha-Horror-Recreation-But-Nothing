56a126a153e87af9f34aafad5b749d134315f9f0
Lily <egzozu.be.bas@gmail.com>
2024-06-29 21:24:22 +0300

    somce changes cuz yes

--- mods/custom_events/WindowMove.lua
+++ mods/custom_events/WindowMove.lua
@@ -9,11 +9,11 @@ function onEvent(name, value1, value2)
         if buildTarget == 'android' or buildTarget == 'ios' then --nerd
         setPropertyFromClass('openfl.Lib', 'application.window.width', width/getRandomInt(0.001,2.999)) 
 	    setPropertyFromClass('openfl.Lib', 'application.window.height', height/getRandomInt(0.001,2.999)) 
-		else
-		setPropertyFromClass('openfl.Lib', 'application.window.width', width/2.25) 
-		setPropertyFromClass('openfl.Lib', 'application.window.height', height/2.25)
-		end
+	    else
 	    setPropertyFromClass('openfl.Lib', 'application.window.x', getRandomInt(0,700)) 
-        setPropertyFromClass('openfl.Lib', 'application.window.y', getRandomInt(0,350))
+        setPropertyFromClass('openfl.Lib', 'application.window.y', getRandomInt(0,350)) 
+	    setPropertyFromClass('openfl.Lib', 'application.window.width', width/2.25) 
+	    setPropertyFromClass('openfl.Lib', 'application.window.height', height/2.25)
+        end 
     end
 end
--- mods/scripts/GhostNotes.lua
+++ mods/scripts/GhostNotes.lua
@@ -1,55 +1,70 @@
 local silhouetteShit = require('mods/custom_events/Silhouette')
 
-function goodNoteHit(i,d,t,s)
-  if _G['boyfriendGhostData.strumTime'] == getPropertyFromGroup('notes',i,'strumTime') and not s and not getPropertyFromGroup('notes',i,'gfNote') then
+function onCreate()
+  precacheImage(char..'.imageFile')
+end
+
+function goodNoteHit(id, direction, noteType, isSustainNote)
+  if _G['boyfriendGhostData.strumTime'] == getPropertyFromGroup('notes', id, 'strumTime') and not isSustainNote and not getPropertyFromGroup('notes',id,'gfNote') then
     createGhost('boyfriend')
-  elseif not s and not getPropertyFromGroup('notes',i,'gfNote') then
-    _G['boyfriendGhostData.strumTime'] = getPropertyFromGroup('notes',i,'strumTime')
+  end
+
+  if not isSustainNote and not getPropertyFromGroup('notes',id,'gfNote') then
+    _G['boyfriendGhostData.strumTime'] = getPropertyFromGroup('notes', id, 'strumTime')
     updateGData('boyfriend')
-  elseif _G['gfGhostData.strumTime'] == getPropertyFromGroup('notes',i,'strumTime') and not s and getPropertyFromGroup('notes',i,'gfNote') then
+  end
+
+  if _G['gfGhostData.strumTime'] == getPropertyFromGroup('notes',id,'strumTime') and not isSustainNote and getPropertyFromGroup('notes',id,'gfNote') then
     createGhost('gf')
-  elseif not s and getPropertyFromGroup('notes',i,'gfNote') then
+  end
+
+  if not isSustainNote and getPropertyFromGroup('notes',id,'gfNote') then
     _G['gfGhostData.strumTime'] = getPropertyFromGroup('notes',i,'strumTime')
     updateGData('gf')
   end
 end
 
-function opponentNoteHit(i,d,t,s)
-  if _G['dadGhostData.strumTime'] == getPropertyFromGroup('notes',i,'strumTime') and not s and not getPropertyFromGroup('notes',i,'gfNote') then
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+  if _G['dadGhostData.strumTime'] == getPropertyFromGroup('notes', id, 'strumTime') and not isSustainNote and not getPropertyFromGroup('notes',i,'gfNote') then
     createGhost('dad')
-  elseif not s and not getPropertyFromGroup('notes',i,'gfNote') then
-    _G['dadGhostData.strumTime'] = getPropertyFromGroup('notes',i,'strumTime')
+  end
+
+  if not isSustainNote and not getPropertyFromGroup('notes',i,'gfNote') then
+    _G['dadGhostData.strumTime'] = getPropertyFromGroup('notes', id, 'strumTime')
     updateGData('dad')
-  elseif _G['gfGhostData.strumTime'] == getPropertyFromGroup('notes',i,'strumTime') and not s and getPropertyFromGroup('notes',i,'gfNote') then
+  end
+
+  if _G['gfGhostData.strumTime'] == getPropertyFromGroup('notes',id,'strumTime') and not isSustainNote and getPropertyFromGroup('notes',id,'gfNote') then
     createGhost('gf')
-  elseif not s and getPropertyFromGroup('notes',i,'gfNote')then
+  end
+
+  if not isSustainNote and getPropertyFromGroup('notes',id,'gfNote') then
     _G['gfGhostData.strumTime'] = getPropertyFromGroup('notes',i,'strumTime')
-    updateGData('gf')    
+    updateGData('gf')
   end
 end
 
 function createGhost(char)
   songPos = getSongPosition()
-  makeAnimatedLuaSprite(char..'Ghost'..songPos,getProperty(char..'.imageFile'),getProperty(char..'.x'),getProperty(char..'.y'))
-  addLuaSprite(char..'Ghost'..songPos,false)
+  makeAnimatedLuaSprite(char..'Ghost'..songPos, getProperty(char..'.imageFile'),getProperty(char..'.x'),getProperty(char..'.y'))
+  addLuaSprite(char..'Ghost'..songPos, false)
   setProperty(char..'Ghost'..songPos..'.scale.x',getProperty(char..'.scale.x'))
   setProperty(char..'Ghost'..songPos..'.scale.y',getProperty(char..'.scale.y'))
-  setProperty(char..'Ghost'..songPos..'.flipX',getProperty(char..'.flipX'))
+  setProperty(char..'Ghost'..songPos..'.flipX', getProperty(char..'.flipX'))
   if inSilhouette then
-  setProperty(char..'Ghost'..songPos..'.color',000000)
-  else
-  end
-  setProperty(char..'Ghost'..songPos..'.alpha',1)
-  doTweenAlpha(char..'Ghost'..songPos..'delete',char..'Ghost'..songPos,0,0.4)
-  setProperty(char..'Ghost'..songPos..'.animation.frameName',_G[char..'GhostData.frameName'])
-  setProperty(char..'Ghost'..songPos..'.offset.x',_G[char..'GhostData.offsetX'])
-  setProperty(char..'Ghost'..songPos..'.offset.y',_G[char..'GhostData.offsetY'])
-  setObjectOrder(char..'Ghost'..songPos,getObjectOrder(char..'Group')-1)
+    setProperty(char..'Ghost'..songPos..'.color',000000)
+  else end
+  setProperty(char..'Ghost'..songPos..'.alpha', 1)
+  doTweenAlpha(char..'Ghost'..songPos..'delete', char..'Ghost'..songPos, 0, 0.4)
+  setProperty(char..'Ghost'..songPos..'.animation.frameName', _G[char..'GhostData.frameName'])
+  setProperty(char..'Ghost'..songPos..'.offset.x', _G[char..'GhostData.offsetX'])
+  setProperty(char..'Ghost'..songPos..'.offset.y', _G[char..'GhostData.offsetY'])
+  setObjectOrder(char..'Ghost'..songPos, getObjectOrder(char..'Group')-1)
 end
 
 function onTweenCompleted(tag)
-  if (tag:sub(#tag- 5,#tag)) == 'delete' then
-    removeLuaSprite(tag:sub(1,#tag - 6),true)
+  if (tag:sub(#tag- 5, #tag)) == 'delete' then
+    removeLuaSprite(tag:sub(1, #tag - 6), true)
   end 
 end
 
