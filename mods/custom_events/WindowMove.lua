af5d8c4722426cc1fc6b78fbd60d02610827e2a0
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2024-07-11 16:28:56 +0300

    Crisis Fix

--- mods/custom_events/WindowMove.lua
+++ mods/custom_events/WindowMove.lua
@@ -7,8 +7,8 @@ function onEvent(name, value1, value2)
     if name == "WindowMove" then
         setPropertyFromClass('openfl.Lib', 'application.window.fullscreen', false)
         if buildTarget == 'android' or buildTarget == 'ios' then --nerd
-        setPropertyFromClass('openfl.Lib', 'application.window.width', width/getRandomInt(0.001,2.999)) 
-	    setPropertyFromClass('openfl.Lib', 'application.window.height', height/getRandomInt(0.001,2.999)) 
+        setPropertyFromClass('openfl.Lib', 'application.window.width', width/getRandomFloat(0.001,2.999)) 
+	    setPropertyFromClass('openfl.Lib', 'application.window.height', height/getRandomFloat(0.001,2.999)) 
 	    else
 	    setPropertyFromClass('openfl.Lib', 'application.window.x', getRandomInt(0,700)) 
         setPropertyFromClass('openfl.Lib', 'application.window.y', getRandomInt(0,350)) 
